1f954608247c3a202f83394ddcf82ebdba83c073
lkt2918 <32476469+lkt2918@users.noreply.github.com>
2021-03-19 20:27:45 -0500

    switchliaght

--- src/App.js
+++ src/App.js
@@ -1,91 +1,50 @@
-import Counter from './Component/Counter';
 import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  plusHandler = (counter) => {
-    const newCounters = [...this.state.counters];
-    const index = newCounters.indexOf(counter);
-    newCounters[index].value++;
-    this.setState({ newCounters});
-  };
-  cutHandler = (counter) => {
-    const newCounters = [...this.state.counters];
-    const index = newCounters.indexOf(counter);
-    newCounters[index].value--;
-    this.setState({ newCounters});
-  };
-  deleteHandler = (counter) => {
-    const newCounters = this.state.counters.filter(c => c.id !== counter.id);
-    this.setState({newCounters });
-  };
-  resetHandler = () => {
-    const newState = this.state.counters.map((c) => {
-      c.value = 0
-      return c;
-    });
-    this.setState({newState});
-  };
-
-  restartHandler = () => {
-    window.location.reload();
-  };
-  render() {
-  return (
-    <div>
-      <nav class="navbar navbar-light" >
-      <div className="navbar-brand">
-        <i className="fa fa-shopping-cart  fa-lg m-2" />
-        <span class="badge rounded-pill bg-info text-dark m-2">
-          {      
-            this.state.counters.filter((c) => c.value > 0).length
-          }
-          </span>
-        Items
-        </div>
-      </nav>
-      <main className="contailer">
-      <button
-          className="btn btn-success m-2"
-          onClick={this.resetHandler}
-          disabled={this.state.counters.length === 0 ? "disabled" : ""}
-        >
-          <i className="fa fa-refresh" aria-hidden="true" />
-        </button>
-        <button
-          className="btn btn-primary m-2"
-          onClick={this.restartHandler}
-          disabled={this.state.counters.length !== 0 ? "disabled" : ""}
-        >
-          <i className="fa fa-recycle" aria-hidden="true" />
-        </button>
-        <div>
-          
-        {this.state.counters.map(counter => (
-          <Counter 
-            key={counter.id}
-            counter={counter}
-            plusHandler={this.plusHandler}
-            cutHandler={this.cutHandler}
-            deleteHandler={this.deleteHandler}
-          />
-        ))
-        }
-        </div>
-      </main>
-       
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      switchOn: false
+    }
+    
+    this.toggleSwitch = this.toggleSwitch.bind(this);
   }
-};
 
+  toggleSwitch() {
+    this.setState({ switchOn: !this.state.switchOn });
+  }
+
+  render() {
+    return (
+      <div className="layout">
+        <Switch isOn={this.state.switchOn} onSwitched={this.toggleSwitch} />
+        <Bulb isOn={this.state.switchOn} />
+      </div>
+    )
+  }
+}
+class Switch extends React.Component {
+  render() {
+      var switchClass = this.props.isOn ? "cube-switch active" : "cube-switch";
+      return (
+        <div className={switchClass}>
+          <span className="switch" onClick={this.props.onSwitched}>
+            <span className="switch-state off">Off</span>
+            <span className="switch-state on">On</span>
+          </span>
+        </div>
+      );
+  }
+}
+
+class Bulb extends React.Component {
+render() {
+  let lightClass = this.props.isOn ? "light-bulb on" : "light-bulb off";
+  return (
+    <div className={lightClass}></div>
+  )
+}
+}
 export default App;
